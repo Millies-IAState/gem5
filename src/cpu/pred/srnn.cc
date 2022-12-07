721732ca6f2434be00351a631742a1258ac49871
mitchillies-personal <Millies@iastate.edu>
2022-12-07 08:43:40 -0600

    Incorrect Sign

--- src/cpu/pred/srnn.cc
+++ src/cpu/pred/srnn.cc
@@ -129,13 +129,13 @@ SrnnBP::SrnnBP(const SrnnBPParams &params)
             }
             else if(localPHTBits == INT_BIT_COUNT - 1)
             {
-                randW = (generateRandomInt() % (INT32_MAX)) - weightMin;
-                randU = (generateRandomInt() % (INT32_MAX)) - weightMin;
+                randW = (generateRandomInt() % (INT32_MAX)) + weightMin;
+                randU = (generateRandomInt() % (INT32_MAX)) + weightMin;
             }
             else
             {
-                randW = (generateRandomInt() % (weightMax + weightMax)) - weightMin;
-                randU = (generateRandomInt() % (weightMax + weightMax)) - weightMin;
+                randW = (generateRandomInt() % (weightMax + weightMax)) + weightMin;
+                randU = (generateRandomInt() % (weightMax + weightMax)) + weightMin;
             }
 
             DPRINTF(SrnnBPDB, "Indexing W and U: %i\r\n",j);
