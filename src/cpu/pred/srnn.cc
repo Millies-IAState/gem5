aef98280794fd427eac3177434a242a43e70fa93
mitchillies-personal <Millies@iastate.edu>
2022-12-08 05:43:37 -0600

    missing character

--- src/cpu/pred/srnn.cc
+++ src/cpu/pred/srnn.cc
@@ -245,7 +245,7 @@ SrnnBP::lookup(ThreadID tid, Addr branch_addr, void * &bp_history)
 
             DPRINTF(SrnnBPDB, "Inputs S - Index1: %li Value1: %lli Index2: %li Value2: %lli, U - Index: %li Value: %lli\n",
             index1, sValues[index1],index2,sValues[index2],uIndex, uValues[uIndex]);
-            DPRINTF(SrnnBPDB, "%lli + (%lli * %lli) = ",sValues[index2], Values[uIndex], sValues[index1]);
+            DPRINTF(SrnnBPDB, "%lli + (%lli * %lli) = ",sValues[index2], uValues[uIndex], sValues[index1]);
 
             sValues[i] = (sValues[index2] + (((int64_t)uValues[uIndex]) * sValues[index1]));
             DPRINTF(SrnnBPDB, "%lli in index %li\n",sValues[index1],i);
